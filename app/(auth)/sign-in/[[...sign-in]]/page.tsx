59547cba6e6c50bf23e359c628c79f7354db0bdb
Adinath Shelke <shelkeadinath3@gmail.com>
2024-12-08 01:44:12 +0530

    feat(overview): Created period selecctor for history

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return <SignIn />
+export default async function Page() {
+  const user = await currentUser();
+  if (user) {
+    return redirect("/");
+  }
+  return <SignIn />;
 }
---
+++ app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { GetHistoryPeriodsResponseType } from "@/app/api/history-periods/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import {
+  Select,
+  SelectItem,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Period, Timeframe } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+
+interface Props {
+  period: Period;
+  setperiod: (period: Period) => void;
+  timeframe: Timeframe;
+  settimeframe: (timeframe: Timeframe) => void;
+}
+const HistoryPeriodSelector = ({
+  period,
+  setperiod,
+  timeframe,
+  settimeframe,
+}: Props) => {
+  const historyPeriods = useQuery<GetHistoryPeriodsResponseType>({
+    queryKey: ["overview", "history", "periods"],
+    queryFn: () => fetch("/api/history-periods").then((res) => res.json()),
+  });
+  return (
+    <div className="flex flex-wrap items-center gap-4">
+      <SkeletonWrapper isLoading={historyPeriods.isFetching} fullWidth={false}>
+        <Tabs
+          value={timeframe}
+          onValueChange={(value) => settimeframe(value as Timeframe)}
+        >
+          <TabsList>
+            <TabsTrigger value="year">Year</TabsTrigger>
+            <TabsTrigger value="month">Month</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </SkeletonWrapper>
+      <div className="flex flex-wrap items-center gap-2">
+        <SkeletonWrapper isLoading={historyPeriods.isFetching}>
+          <YearSelector
+            period={period}
+            setperiod={setperiod}
+            years={historyPeriods.data || []}
+          />
+        </SkeletonWrapper>
+        {timeframe === "month" && (
+          <SkeletonWrapper
+            isLoading={historyPeriods.isFetching}
+            fullWidth={false}
+          >
+            <MonthSelector period={period} setperiod={setperiod} />
+          </SkeletonWrapper>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HistoryPeriodSelector;
+
+function MonthSelector({
+  period,
+  setperiod,
+}: {
+  period: Period;
+  setperiod: (period: Period) => void;
+}) {
+  return (
+    <Select
+      value={period.month.toString()}
+      onValueChange={(value) =>
+        setperiod({ year: period.year, month: parseInt(value) })
+      }
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((month) => {
+          const monthStr = new Date(period.year, month, 1).toLocaleString(
+            "default",
+            { month: "long" }
+          );
+          return (
+            <SelectItem key={month} value={month.toString()}>
+              {monthStr}
+            </SelectItem>
+          );
+        })}
+      </SelectContent>
+    </Select>
+  );
+}
+
+function YearSelector({
+  period,
+  setperiod,
+  years,
+}: {
+  period: Period;
+  setperiod: (period: Period) => void;
+  years: GetHistoryPeriodsResponseType;
+}) {
+  return (
+    <Select
+      value={period.year.toString()}
+      onValueChange={(value) =>
+        setperiod({ month: period.month, year: parseInt(value) })
+      }
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        {years.map((year) => (
+          <SelectItem key={year} value={year.toString()}>
+            {year}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
